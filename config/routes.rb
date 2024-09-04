80c9d6cfdd8ade1f17f8b349a60958c4e5ed2b2c
Mooreandmoree <131541515+Mooreandmoree@users.noreply.github.com>
2024-09-04 13:37:07 +0100

    "Deleted file hello.rb"

---
+++ config/routes.rb
@@ -0,0 +1,14 @@
+Rails.application.routes.draw do
+  devise_for :users
+  resources :trees
+  get 'home/index'
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+   root "home#index"
+
+   devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+   end
+
+end
